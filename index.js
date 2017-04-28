32e346dd14a44a9707a2abb51057ea5b4e01e262
Henrique <henriquebjr@gmail.com>
2017-04-28 00:06:04 -0300

    request with proxy

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 3001;
+var Http = require('http');
 
 app.use(bodyParser.json());
 
@@ -16,3 +17,16 @@ app.post('', function(request, response) {
 var server = app.listen(port, function() {
   console.log("Server it's up and running at http://localhost:" + port);
 })
+
+var proxied = Http.request({
+  host: '121.8.98.202',
+  port: 8080,
+  method: 'GET',
+  path: 'http://www.estrela.com.br//'
+}, function(response){
+  response.on('data', function(data){
+    console.log(data.toString());
+  });
+});
+
+proxied.end();
